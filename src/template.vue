84c117f9cb8f901061ae24e917a6b3ede9a1d88f
娄通略 <1746299291@qq.com>
2019-11-06 16:39:22 +0800

    增加template.vue

---
+++ src/template.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='app'></div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+
+    };
+  },
+  methods:{
+
+  },
+  computed:{
+
+  },
+  components: {
+
+  }
+}
+</script>
+<style scoped lang='scss'>
+
+</style>
